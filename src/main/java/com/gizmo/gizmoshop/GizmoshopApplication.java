4de5f29649c05be9f8f344e74c2820c73d69beb0
THINHLC <thinhvt213@gmail.com>
2024-10-06 14:12:34 +0700

    first commit

---
+++ src/main/java/com/gizmo/gizmoshop/GizmoshopApplication.java
@@ -0,0 +1,13 @@
+package com.gizmo.gizmoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GizmoshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GizmoshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gizmo/gizmoshop/GizmoshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gizmo.gizmoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GizmoshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
