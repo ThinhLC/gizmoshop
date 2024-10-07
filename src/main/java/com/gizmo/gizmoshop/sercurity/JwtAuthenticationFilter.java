47aa2ab9adb506c1262c24b1d22d9f6ad2ba77ee
THINHLC <thinhvt213@gmail.com>
2024-10-07 19:15:19 +0700

    change entity

---
+++ src/main/java/com/gizmo/gizmoshop/sercurity/JwtAuthenticationFilter.java
@@ -0,0 +1,9 @@
+package com.gizmo.gizmoshop.sercurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationFilter {
+
+}
---
+++ src/main/java/com/gizmo/gizmoshop/sercurity/SpringSecurityConfig.java
@@ -0,0 +1,12 @@
+package com.gizmo.gizmoshop.sercurity;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+}
