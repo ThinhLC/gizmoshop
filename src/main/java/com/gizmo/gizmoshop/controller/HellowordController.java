662b6e5ac618df265bad891cbe7e97f32a729ffe
THINHLC <thinhvt213@gmail.com>
2024-10-09 17:32:18 +0700

    change entity

--- src/main/java/com/gizmo/gizmoshop/controller/HellowordController.java
+++ src/main/java/com/gizmo/gizmoshop/controller/HellowordController.java
@@ -4,11 +4,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class HellowordController {
 
     @PreAuthorize("permitAll()")
