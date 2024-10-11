14919d534b6a475b5901d8eeb70e30d89de5bd38
Trishoangg <108757664+Trishoangg@users.noreply.github.com>
2024-10-11 20:08:19 +0700

    changed by tri

--- src/main/java/com/gizmo/gizmoshop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gizmo/gizmoshop/exception/GlobalExceptionHandler.java
@@ -39,14 +39,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ResponseEntity<ResponseWrapper<Void>> handleAccessDeniedException(AccessDeniedException ex) {
-<<<<<<< HEAD
         // Tạo một đối tượng ResponseWrapper với các tham số hợp lệ
         ResponseWrapper<Void> response = new ResponseWrapper<>(HttpStatus.FORBIDDEN, "Access denied: " + ex.getMessage(), null);
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
-    }
-
-=======
-        ResponseWrapper<Void> response = new ResponseWrapper<>(HttpStatus.FORBIDDEN, "Không đủ quyền truy cập", null);
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
     }
 
@@ -56,6 +50,4 @@ public class GlobalExceptionHandler {
         ResponseWrapper<Void> response = new ResponseWrapper<>(HttpStatus.FORBIDDEN, "Tài khoản đã tồn tại", null);
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
     }
-
->>>>>>> 4c40860f7c07efedd4f702a9c9c844655679bce4
 }
