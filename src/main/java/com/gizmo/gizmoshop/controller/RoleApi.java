89e29117c56185da0d252ae534b2ade83e6c6888
Trishoangg <108757664+Trishoangg@users.noreply.github.com>
2024-10-14 19:22:25 +0700

    changed by tri

--- src/main/java/com/gizmo/gizmoshop/controller/RoleApi.java
+++ src/main/java/com/gizmo/gizmoshop/controller/RoleApi.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/public")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class RoleApi {
 
     private final RoleService roleService;
