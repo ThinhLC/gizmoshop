5730bfb13e4642232558df8cf3282c3433ef9135
Trishoangg <108757664+Trishoangg@users.noreply.github.com>
2024-10-15 20:39:19 +0700

    changed by tri

--- src/main/java/com/gizmo/gizmoshop/controller/AccountController.java
+++ src/main/java/com/gizmo/gizmoshop/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.gizmo.gizmoshop.controller;
 import com.gizmo.gizmoshop.dto.reponseDto.AccountResponse;
 import com.gizmo.gizmoshop.dto.reponseDto.ResponseWrapper;
 import com.gizmo.gizmoshop.dto.requestDto.AccountRequest;
+import com.gizmo.gizmoshop.dto.requestDto.EmailUpdateRequest;
+import com.gizmo.gizmoshop.dto.requestDto.OtpVerificationRequest;
 import com.gizmo.gizmoshop.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -29,13 +31,23 @@ public class AccountController {
         AccountResponse updatedAccount = accountService.updateLoggedInAccount(accountRequest);
         return ResponseEntity.ok(new ResponseWrapper<>(HttpStatus.OK, "Cập nhật tài khoản thành công", updatedAccount));
     }
-
-    @PostMapping("/account/update-email")
+    // Gửi mã OTP cho việc cập nhật email
+    @PostMapping("/account/email/otp")
     @PreAuthorize("isAuthenticated()") // Yêu cầu tài khoản phải đăng nhập
-    public ResponseEntity<ResponseWrapper<String>> updateEmail(
-            @RequestParam String newEmail) {
-
-        String response = accountService.updateEmail(newEmail);
-        return ResponseEntity.ok(new ResponseWrapper<>(HttpStatus.OK, "Cập nhật email thành công", response));
+    public ResponseEntity<ResponseWrapper<Void>> sendOtpForEmailUpdate(@RequestBody EmailUpdateRequest request) {
+        accountService.sendOtpForEmailUpdate(request);
+        return ResponseEntity.ok(new ResponseWrapper<>(HttpStatus.OK, "Gửi mã OTP cho email mới thành công", null));
     }
+
+    // Xác thực mã OTP và cập nhật email
+    @PutMapping("/account/email/verify")
+    @PreAuthorize("isAuthenticated()") // Yêu cầu tài khoản phải đăng nhập
+    public ResponseEntity<ResponseWrapper<Void>> verifyOtpAndUpdateEmail(
+            @RequestBody OtpVerificationRequest request) {
+        accountService.verifyOtpAndUpdateEmail(request);
+
+        // Trả về phản hồi khi thành công
+        return ResponseEntity.ok(new ResponseWrapper<>(HttpStatus.OK, "Cập nhật email thành công", null));
+    }
+
 }
