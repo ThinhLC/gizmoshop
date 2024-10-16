67d40b1a45c118ae0ad9b5c29f6f3c22c6aee96d
Trishoangg <108757664+Trishoangg@users.noreply.github.com>
2024-10-16 20:21:21 +0700

    comment imageapi

--- src/main/java/com/gizmo/gizmoshop/controller/ImageApi.java
+++ src/main/java/com/gizmo/gizmoshop/controller/ImageApi.java
@@ -4,6 +4,7 @@ package com.gizmo.gizmoshop.controller;
 import com.gizmo.gizmoshop.dto.reponseDto.ResponseWrapper;
 
 import com.gizmo.gizmoshop.service.Image.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
@@ -26,6 +27,10 @@ public class ImageApi {
 
     @PostMapping("/image/upload/{type}")
     @PreAuthorize("permitAll()")
+    @Operation(summary = "Đây là phần đẩy hình ảnh lên",
+            description = "API này được dùng để đẩy hỉnh ảnh lên, type ở đây có thể được sử dụng khi có sự thay thế cho nhiều phần khác nhau," +
+                    "type có thể được thay thế bằng các phần như là account, prodcut, productimage,category",
+            tags = {"imageapi"})
     public ResponseEntity<ResponseWrapper<String>> uploadImage(
             @RequestParam("image") MultipartFile image,
             @PathVariable("type") String type) throws IOException {
@@ -35,6 +40,10 @@ public class ImageApi {
 
     @DeleteMapping("/image/delete/{type}")
     @PreAuthorize("permitAll()")
+    @Operation(summary = "Đây là phần xóa hình ảnh",
+            description = "API này được dùng để đẩy hỉnh ảnh lên, type ở đây có thể được sử dụng khi có sự thay thế cho nhiều phần khác nhau," +
+                    "type có thể được thay thế bằng các phần như là account, prodcut, productimage,category",
+            tags = {"imageapi"})
     public ResponseEntity<ResponseWrapper<String>> deleteImage(@RequestParam("imageName") String imageName, @PathVariable("type") String type) throws IOException {
         imageService.deleteImage(imageName, type);
         return ResponseEntity.ok(new ResponseWrapper<>(HttpStatus.OK, "Hình ảnh đã được xóa thành công", imageName));
@@ -42,6 +51,10 @@ public class ImageApi {
 
     @GetMapping("/image/load/{type}")
     @PreAuthorize("permitAll()")
+    @Operation(summary = "Đây là phần tải hình ảnh",
+            description = "API này được dùng để đẩy hỉnh ảnh lên, type ở đây có thể được sử dụng khi có sự thay thế cho nhiều phần khác nhau," +
+                    "type có thể được thay thế bằng các phần như là account, prodcut, productimage,category",
+            tags = {"imageapi"})
     public ResponseEntity<byte[]> loadImage(@RequestParam("imageName") String imageName, @PathVariable("type") String type) throws IOException {
         byte[] imageData = imageService.loadImageAsResource(imageName, type);
         return ResponseEntity.ok().body(imageData);
