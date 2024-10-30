2efd1a7275d5e9d7ee49b81b73dd1759f70ae7d2
THINHLC <thinhvt213@gmail.com>
2024-10-30 17:30:49 +0700

    Cập nhật

--- src/main/java/com/gizmo/gizmoshop/dto/reponseDto/ProductStatusResponse.java
+++ src/main/java/com/gizmo/gizmoshop/dto/reponseDto/ProductStatusResponse.java
@@ -1,11 +1,9 @@
 package com.gizmo.gizmoshop.dto.reponseDto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
+@Builder
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
