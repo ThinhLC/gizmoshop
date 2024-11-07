d2d0ab1903473acc7149ba8536aa2604b86a3040
TienDungVo <dung81votien1872004@gmail.com>
2024-11-07 10:00:38 +0700

    Dũng commit

---
+++ src/main/java/com/gizmo/gizmoshop/dto/reponseDto/AddressAccountResponse.java
@@ -0,0 +1,28 @@
+package com.gizmo.gizmoshop.dto.reponseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class AddressAccountResponse {
+    private Long id;
+    private String fullname;
+    private String specificAddress;
+    private String sdt;
+    private String city;
+    private String district;
+    private String commune;
+    private String longitude;
+    private String latitude;
+    private Boolean deleted;
+    private Long accountId;
+
+    public AddressAccountResponse(Long id) {
+        this.id = id;
+    }
+}
