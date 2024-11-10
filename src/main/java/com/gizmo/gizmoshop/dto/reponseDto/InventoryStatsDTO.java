3f82a79314374dfe29bf84fb58e241ae0e8c7f67
thansex000 <loclop3@gmail.com>
2024-11-10 16:39:58 +0700

    config api InventoryStats

--- src/main/java/com/gizmo/gizmoshop/dto/reponseDto/InventoryStatsDTO.java
+++ src/main/java/com/gizmo/gizmoshop/dto/reponseDto/InventoryStatsDTO.java
@@ -1,7 +1,9 @@
 package com.gizmo.gizmoshop.dto.reponseDto;
 
+import com.gizmo.gizmoshop.excel.ExcludeFromExport;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
@@ -12,5 +14,15 @@ import java.util.List;
 public class InventoryStatsDTO {
     private Long id;
     private String name;
+    private String city;
+    private String district;
+    private String commune;
+    private String latitude;
+    private String longitude;
+    private Boolean active;
+    @ExcludeFromExport
+    private LocalDateTime createdAt;
+    @ExcludeFromExport
+    private LocalDateTime updatedAt;
     private List<ProductInventoryResponse> productInventory;
 }
