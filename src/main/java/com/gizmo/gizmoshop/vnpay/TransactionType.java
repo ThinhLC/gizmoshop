6209923a4e875475bd96b41d15d2f5852e10fd10
haihaycode <nam.d02th@gmail.com>
2024-11-19 18:04:34 +0700

    enum

--- src/main/java/com/gizmo/gizmoshop/vnpay/TransactionType.java
+++ src/main/java/com/gizmo/gizmoshop/vnpay/TransactionType.java
@@ -1,8 +1,9 @@
 package com.gizmo.gizmoshop.vnpay;
 
 public enum TransactionType {
-    ORDER_PAYMENT("order_payment"),    // Thanh toán cho đơn hàng
-    ACCOUNT_TOPUP("account_topup");    // Nộp tiền vào tài khoản
+    ORDER_PAYMENT("order_payment"),       // Thanh toán cho đơn hàng
+    ACCOUNT_TOPUP("account_topup"),       // Nộp tiền vào tài khoản
+    SUPPLIER_REGISTRATION("supplier_registration"); // Nộp tiền để đăng ký nhà cung cấp
 
     private final String type;
 
